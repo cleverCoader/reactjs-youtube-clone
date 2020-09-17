da2eb134d8a4d843c9b288d2617ef9fe99851f4c
cleverCoader <adventure.developer45@gmail.com>
2020-09-18 02:59:49 +0530

    Completely better looking Static youtube is Ready now @ 2:59 am of 18th sept 2020

--- src/RecomendedVideos.js
+++ src/RecomendedVideos.js
@@ -1,12 +1,209 @@
-import React from 'react'
+import React from 'react';
+import VideoCard from './VideoCard';
+
 import './RecomendedVideos.css';
+
+//image, title, channel, views, timeStamp, channelImage
 
 function RecomendedVideos() {
     return (
         <div className="recomendedvideos" >
             <h2> Recommended</h2>
-            <div recommendedVideos__videos>
-                {/* <Video /> */}
+            <div className="recommendedVideos__videos">
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://ae01.alicdn.com/kf/HTB13hxTKFXXXXa5XpXXq6xXFXXXR/Small-size-rose-2014-new-arrival-artificial-silk-flowers-25-pieces-a-bouquet-artificial-roses-special.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlkz47tiHDoibyIlvtpSvh2qON3TFf295Pbg&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz51WKfP3Ak53ElZ6v6mJ2aXEWuv-zGg9Aw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://i.pinimg.com/736x/d5/d4/69/d5d4696f00821d943c2431ebce30d34a.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://ae01.alicdn.com/kf/HTB13hxTKFXXXXa5XpXXq6xXFXXXR/Small-size-rose-2014-new-arrival-artificial-silk-flowers-25-pieces-a-bouquet-artificial-roses-special.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlkz47tiHDoibyIlvtpSvh2qON3TFf295Pbg&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz51WKfP3Ak53ElZ6v6mJ2aXEWuv-zGg9Aw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://i.pinimg.com/736x/d5/d4/69/d5d4696f00821d943c2431ebce30d34a.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://ae01.alicdn.com/kf/HTB13hxTKFXXXXa5XpXXq6xXFXXXR/Small-size-rose-2014-new-arrival-artificial-silk-flowers-25-pieces-a-bouquet-artificial-roses-special.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlkz47tiHDoibyIlvtpSvh2qON3TFf295Pbg&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz51WKfP3Ak53ElZ6v6mJ2aXEWuv-zGg9Aw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://i.pinimg.com/736x/d5/d4/69/d5d4696f00821d943c2431ebce30d34a.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://ae01.alicdn.com/kf/HTB13hxTKFXXXXa5XpXXq6xXFXXXR/Small-size-rose-2014-new-arrival-artificial-silk-flowers-25-pieces-a-bouquet-artificial-roses-special.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlkz47tiHDoibyIlvtpSvh2qON3TFf295Pbg&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz51WKfP3Ak53ElZ6v6mJ2aXEWuv-zGg9Aw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://i.pinimg.com/736x/d5/d4/69/d5d4696f00821d943c2431ebce30d34a.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://ae01.alicdn.com/kf/HTB13hxTKFXXXXa5XpXXq6xXFXXXR/Small-size-rose-2014-new-arrival-artificial-silk-flowers-25-pieces-a-bouquet-artificial-roses-special.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlkz47tiHDoibyIlvtpSvh2qON3TFf295Pbg&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz51WKfP3Ak53ElZ6v6mJ2aXEWuv-zGg9Aw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://i.pinimg.com/736x/d5/d4/69/d5d4696f00821d943c2431ebce30d34a.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://ae01.alicdn.com/kf/HTB13hxTKFXXXXa5XpXXq6xXFXXXR/Small-size-rose-2014-new-arrival-artificial-silk-flowers-25-pieces-a-bouquet-artificial-roses-special.jpg"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQlkz47tiHDoibyIlvtpSvh2qON3TFf295Pbg&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvz51WKfP3Ak53ElZ6v6mJ2aXEWuv-zGg9Aw&usqp=CAU"
+                />
+                <VideoCard
+                    title="Beautiful flowers "
+                    views="10M views"
+                    channel="Flower Shows"
+                    timeStamp="1 min ago"
+                    channelImage="https://avatars0.githubusercontent.com/u/71284942?s=460&u=042477abf4c5020157b30e8506b311a02d37f492&v=4"
+                    image="https://i.pinimg.com/736x/d5/d4/69/d5d4696f00821d943c2431ebce30d34a.jpg"
+                />
+                
+                
             </div>
         </div>
     )
---
+++ src/VideoCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './VideoCard.css';
+
+import Avatar from '@material-ui/core/Avatar';
+
+
+function VideoCard({image, title, channel, views, timeStamp, channelImage}) {
+    return (
+        <div className="videoCard" >
+            <img className="videoCard_image" src={image} alt="" />
+            <div className="videoCard_info">
+                <Avatar className="videoCard_avatar" alt={channel} src={channelImage} />
+                <div className="video_text">
+                    <h4> {title} </h4>
+                    <p> {channel} </p>
+                    <p>
+                        {views} . {timeStamp}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VideoCard;
