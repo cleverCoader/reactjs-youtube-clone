10123eeb9a6f4fd42bb61ffe1ded6af7d23bcd19
cleverCoader <adventure.developer45@gmail.com>
2020-09-18 22:15:58 +0530

    installed react-router and just playing with it

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import RecomendedVideos from './RecomendedVideos';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="app">
-      {/* <h1> Youtube Clone</h1> */}
-      <Header />
       
+      <Router>
+        <Switch>
+        <Route path="/videos">
+            <h1>We are In videos Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>We are In home Page</h1>
+          </Route>
+        </Switch>
+      </Router>
+
+      
+
+      {/* <Header />
       <div className="app_page">
         <Sidebar />
         <RecomendedVideos />
-      </div>
+      </div> */}
 
       
       
